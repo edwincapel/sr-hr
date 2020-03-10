117c63223fa5c4a1915d930d489aa6af85a8220b
edwincapel <edwinrc1026@gmail.com>
2020-03-10 17:30:32 +0800

    change gitignore

---
+++ web-build/precache-manifest.0ef4f594f5342edcbb809653104ec43b.js
@@ -0,0 +1,126 @@
+self.__precacheManifest = [
+  {
+    "revision": "44b9d5579e33817f624a",
+    "url": "/static/js/app.4b638551.chunk.js"
+  },
+  {
+    "revision": "2761cd8426f52ed76a3e",
+    "url": "/static/js/runtime~app.43360584.js"
+  },
+  {
+    "revision": "6683dab09f61c10f28ac",
+    "url": "/static/js/2.cfaf7a52.chunk.js"
+  },
+  {
+    "revision": "717d35697119ea9f545f487012600963",
+    "url": "/3ecb68a4aa672e29cf99.worker.js"
+  },
+  {
+    "revision": "7ac396e34dc6415781266407adb6a515",
+    "url": "/static/media/team.7ac396e3.jpg"
+  },
+  {
+    "revision": "77169ef707cb35fcbc27849dd4a85994",
+    "url": "/static/media/logosr.77169ef7.png"
+  },
+  {
+    "revision": "d0c694b562b2208635f250762cd7fc79",
+    "url": "/serve.json"
+  },
+  {
+    "revision": "7a7bc7ead25db795e58b336f04d2624c",
+    "url": "/favicon.ico"
+  },
+  {
+    "revision": "48c7ed4e7da9792af288a60d7242d615",
+    "url": "/expo-service-worker.js"
+  },
+  {
+    "revision": "ca5d83e2587c12f1ef520b8e0b0dc9e5",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "ef58ff9a14b1871e5232fada458806e5",
+    "url": "/apple/icons/icon_180x180.png"
+  },
+  {
+    "revision": "dd0d8974f196283ab21f6bb647882750",
+    "url": "/apple/icons/icon_192x192.png"
+  },
+  {
+    "revision": "b370d9dab464db3e849d854262b5d2e5",
+    "url": "/apple/icons/icon_512x512.png"
+  },
+  {
+    "revision": "b4c95fe40cc182ee41709b5386a65a60",
+    "url": "/apple/splash/icon_1125x2436.png"
+  },
+  {
+    "revision": "24057a53b4b8207ac6128f5a1858a41f",
+    "url": "/apple/splash/icon_1136x640.png"
+  },
+  {
+    "revision": "d8bfb76284287658cab36f271d2a2a4a",
+    "url": "/apple/splash/icon_1242x2208.png"
+  },
+  {
+    "revision": "46f33d43a6ee160cfb767b7e565d3963",
+    "url": "/apple/splash/icon_1242x2688.png"
+  },
+  {
+    "revision": "ff41747f4f3bb2f9a29c2ee494815852",
+    "url": "/apple/splash/icon_1334x750.png"
+  },
+  {
+    "revision": "79a7a4869dc12a14e286c382d496dfd0",
+    "url": "/apple/splash/icon_1792x828.png"
+  },
+  {
+    "revision": "7e9fc39b1c37a328126323bbe8cd404b",
+    "url": "/apple/splash/icon_2208x1242.png"
+  },
+  {
+    "revision": "1fd86c39fec0a11f2f6b61d5a7af0f39",
+    "url": "/apple/splash/icon_2436x1125.png"
+  },
+  {
+    "revision": "dbbf9209e64340c9d1b78823b8f8abf2",
+    "url": "/apple/splash/icon_2688x1242.png"
+  },
+  {
+    "revision": "df18c0c33e95b50b62f822d65cd9b909",
+    "url": "/apple/splash/icon_640x1136.png"
+  },
+  {
+    "revision": "5d0bcf1c916780f7a20f047bb6d8a30c",
+    "url": "/apple/splash/icon_750x1334.png"
+  },
+  {
+    "revision": "4ed2cf28de535595662c61587b07aa56",
+    "url": "/apple/splash/icon_828x1792.png"
+  },
+  {
+    "revision": "0014d6ba42ae5743d9ee6b637ac2bb92",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e157cdf80400ab9101a77c1182d36c5d",
+    "url": "/static/js/app.4b638551.chunk.js.gz"
+  },
+  {
+    "revision": "b2ecaaec004fff538a49bd30f9de665e",
+    "url": "/static/js/runtime~app.43360584.js.gz"
+  },
+  {
+    "revision": "16709f23227d220b53e235a942bf292c",
+    "url": "/expo-service-worker.js.gz"
+  },
+  {
+    "revision": "eb215e3b161d3f1b197e47cfe71e42cb",
+    "url": "/static/js/2.cfaf7a52.chunk.js.gz"
+  },
+  {
+    "revision": "5102b413184ebebbb47fda251c19a174",
+    "url": "/3ecb68a4aa672e29cf99.worker.js.gz"
+  }
+];
