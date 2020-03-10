803107e4f5623d8d8523e27a419d5a55116d4f03
edwincapel <edwinrc1026@gmail.com>
2020-03-10 17:07:35 +0800

    can submit application

--- src/components/stepOne.tsx
+++ src/components/stepOne.tsx
@@ -1,56 +1,112 @@
 import React, { useState } from "react";
 import { Container, Col, Row } from "reactstrap";
-import { Button, Form, FormGroup, Label, Input } from "reactstrap";
+import { Button, Form, FormGroup, Label, Input, Alert } from "reactstrap";
 import { Props } from "../helpers/helpers"
 
 export default function StepOne(props: Props) {
-  const [firstName, setFirstName] = useState("")
-  const [lastName, setLastName] = useState("");
+  const [errors, setErrors] = useState([]);
+  const [visible, setVisible] = useState(true);
+
+  const onDismiss = () => {
+    setVisible(false);
+  };
 
   const handleChange = (): void => {
-    props.setName(`${firstName} ${lastName}`);
-    props.setStep(2);
+    let customErrors = [];
+
+    if (props.firstName.length === 0) {
+      customErrors.push('First Name: Please enter First Name')
+    }
+
+    if (props.lastName.length === 0) {
+      customErrors.push("Last Name: Please enter Last Name");
+    }
+
+    if (props.firstName.length > 0 && props.lastName.length > 0) {
+      props.setStep(3);
+    }
+    else{
+      setErrors(customErrors);
+      setVisible(true);
+    }
+
   };
 
   return (
     <Container className={"h-100"} fluid>
-      <Row className={"h-100"}>
-        <Col className="d-flex justify-content-center align-items-center">
-          <Form className="w-75">
-            <FormGroup className="mb-2 mr-sm-2 mb-sm-0">
-              <Label for="firstName" className="mr-sm-2">
+      <Row className={"h-100"} style={{ backgroundColor: "#000" }}>
+        <Col className="d-flex justify-content-center align-items-center flex-column">
+          <h1 className="mb-5" style={{ ...H1Style }}>
+            What's your name ?
+          </h1>
+          {errors.length > 0 && (
+            <>
+              {errors.map((error: string, index: number) => {
+                return (
+                  <Alert
+                    key={`${index}`}
+                    color="danger"
+                    className="mb-3 w-75"
+                    isOpen={visible}
+                    toggle={onDismiss}
+                    fade={true}
+                  >
+                    {error}
+                  </Alert>
+                );
+              })}
+            </>
+          )}
+          <Form className="w-75 d-flex justify-content-center align-items-center flex-column">
+            <FormGroup className="mb-2 mr-sm-2 mb-sm-0 w-75">
+              <Label
+                for="firstName"
+                className="mr-sm-2"
+                style={{ ...LabelStyle }}
+              >
                 First Name
               </Label>
               <Input
+                valid={props.firstName.length > 0}
+                value={props.firstName}
                 type="text"
                 name="firstName"
                 id="firstName"
                 placeholder="First Name"
                 onChange={e => {
-                  setFirstName(e.target.value);
+                  props.setFirstName(e.target.value);
                 }}
               />
             </FormGroup>
-            <FormGroup className="mb-2 mr-sm-2 mb-sm-0">
-              <Label for="lastName" className="mr-sm-2">
+            <FormGroup className="mb-2 mr-sm-2 mb-sm-0 w-75 mt-5">
+              <Label
+                for="lastName"
+                className="mr-sm-2"
+                style={{ ...LabelStyle }}
+              >
                 Last Name
               </Label>
               <Input
+                valid={props.lastName.length > 0}
+                value={props.lastName}
                 type="text"
                 name="lastName"
                 id="lastName"
                 placeholder="Last Name"
                 onChange={e => {
-                  setLastName(e.target.value);
+                  props.setLastName(e.target.value);
                 }}
               />
             </FormGroup>
-            <Button onClick={(e)=>{
-                e.preventDefault 
-                handleChange()
+            <Button
+              onClick={e => {
+                e.preventDefault;
+                handleChange();
               }}
+              className={"btn-lg w-50 mt-5"}
+              style={{ ...ButtonStyle }}
             >
-              Submit
+              Next
             </Button>
           </Form>
         </Col>
@@ -58,3 +114,23 @@ export default function StepOne(props: Props) {
     </Container>
   );
 }
+
+const ButtonStyle = {
+  fontFamily: "Montserrat, sans-serif",
+  color: "#000",
+  fontWeight: 500,
+  borderColor: "transparent",
+  backgroundColor: "rgba(255, 172, 8, 0.8)"
+};
+
+const LabelStyle = {
+  fontFamily: "Montserrat, sans-serif",
+  color: "#FFAC08",
+  fontWeight: 600
+};
+
+const H1Style = {
+  fontFamily: "Montserrat, sans-serif",
+  color: "#FFAC08",
+  fontWeight: 600
+};
